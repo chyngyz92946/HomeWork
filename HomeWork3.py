d10d54abde4b881d89b99cd4897def3e26a22948
chyngyz92946 <35916823+chyngyz92946@users.noreply.github.com>
2018-02-03 20:38:18 +0600

    code correct

--- HomeWork3.py
+++ HomeWork3.py
@@ -2,7 +2,28 @@ from MyModuls.moduls import *
 import os
 import datetime
 
+def kill(src, file_name):
+    '''
+    Функция принемает в аргумент путь к папке
+    в которой находиться файл который нужно удалить,
+    второй аргумент имя файла с расширением.
+    '''
+    os.chdir(src)
+    l = os.listdir('.')
+    for i in l:
+        if os.path.isfile(i):
+            print(i)
+            if i == file_name:
+                os.remove(file_name)
+        else:
+            kill(i, file_name)
+            os.chdir('../')
+
+print(kill('.', 'txt.txt'))
+
 print(kill('.', 'txttxt.txt'))
+
+
 while True:
     now = datetime.datetime.now()   # показывает время
     time.sleep(1)   # задержка в 1 секунду
